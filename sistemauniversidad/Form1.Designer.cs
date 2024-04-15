cbf178bbeb93499d0c3059c4087990a49600a49f
Alejandrofdez12 <163462729+Alejandrofdez12@users.noreply.github.com>
2024-04-15 17:30:10 -0600

    Add files via upload

---
+++ sistemauniversidad/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace sistemauniversidad
+{
+    partial class frmIngresodelestudiante
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbDatosEstudiante = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.txtApellido2 = new System.Windows.Forms.TextBox();
+            this.txtApellido1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCedula = new System.Windows.Forms.Label();
+            this.dgvNombreEstudiante = new System.Windows.Forms.DataGridView();
+            this.grbDatosEstudiante.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNombreEstudiante)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grbDatosEstudiante
+            // 
+            this.grbDatosEstudiante.Controls.Add(this.dgvNombreEstudiante);
+            this.grbDatosEstudiante.Controls.Add(this.btnSalir);
+            this.grbDatosEstudiante.Controls.Add(this.BtnLimpiar);
+            this.grbDatosEstudiante.Controls.Add(this.btnGrabar);
+            this.grbDatosEstudiante.Controls.Add(this.txtApellido2);
+            this.grbDatosEstudiante.Controls.Add(this.txtApellido1);
+            this.grbDatosEstudiante.Controls.Add(this.label5);
+            this.grbDatosEstudiante.Controls.Add(this.label4);
+            this.grbDatosEstudiante.Controls.Add(this.dateTimePicker1);
+            this.grbDatosEstudiante.Controls.Add(this.label3);
+            this.grbDatosEstudiante.Controls.Add(this.txtNombre);
+            this.grbDatosEstudiante.Controls.Add(this.txtCedula);
+            this.grbDatosEstudiante.Controls.Add(this.label2);
+            this.grbDatosEstudiante.Controls.Add(this.lblCedula);
+            this.grbDatosEstudiante.Location = new System.Drawing.Point(23, 12);
+            this.grbDatosEstudiante.Name = "grbDatosEstudiante";
+            this.grbDatosEstudiante.Size = new System.Drawing.Size(845, 417);
+            this.grbDatosEstudiante.TabIndex = 0;
+            this.grbDatosEstudiante.TabStop = false;
+            this.grbDatosEstudiante.Text = "Datos del Estudiante";
+            this.grbDatosEstudiante.Enter += new System.EventHandler(this.grbDatosEstudiante_Enter);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(581, 164);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(106, 41);
+            this.btnSalir.TabIndex = 12;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpiar.Location = new System.Drawing.Point(371, 164);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(102, 41);
+            this.BtnLimpiar.TabIndex = 11;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrabar.Location = new System.Drawing.Point(108, 164);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(111, 41);
+            this.btnGrabar.TabIndex = 10;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            // 
+            // txtApellido2
+            // 
+            this.txtApellido2.Location = new System.Drawing.Point(581, 88);
+            this.txtApellido2.Name = "txtApellido2";
+            this.txtApellido2.Size = new System.Drawing.Size(100, 22);
+            this.txtApellido2.TabIndex = 9;
+            // 
+            // txtApellido1
+            // 
+            this.txtApellido1.Location = new System.Drawing.Point(373, 91);
+            this.txtApellido1.Name = "txtApellido1";
+            this.txtApellido1.Size = new System.Drawing.Size(100, 22);
+            this.txtApellido1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(494, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Apellido 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(280, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Apellido 1:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(452, 40);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(288, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha de Nacimiento:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(140, 91);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(140, 40);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(100, 22);
+            this.txtCedula.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // lblCedula
+            // 
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCedula.Location = new System.Drawing.Point(31, 40);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(60, 16);
+            this.lblCedula.TabIndex = 0;
+            this.lblCedula.Text = "Cedula:";
+            // 
+            // dgvNombreEstudiante
+            // 
+            this.dgvNombreEstudiante.AllowUserToAddRows = false;
+            this.dgvNombreEstudiante.AllowUserToDeleteRows = false;
+            this.dgvNombreEstudiante.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvNombreEstudiante.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvNombreEstudiante.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvNombreEstudiante.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNombreEstudiante.Location = new System.Drawing.Point(0, 243);
+            this.dgvNombreEstudiante.Name = "dgvNombreEstudiante";
+            this.dgvNombreEstudiante.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvNombreEstudiante.RowHeadersWidth = 51;
+            this.dgvNombreEstudiante.RowTemplate.Height = 24;
+            this.dgvNombreEstudiante.Size = new System.Drawing.Size(687, 150);
+            this.dgvNombreEstudiante.TabIndex = 13;
+            // 
+            // frmIngresodelestudiante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(897, 441);
+            this.ControlBox = false;
+            this.Controls.Add(this.grbDatosEstudiante);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmIngresodelestudiante";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ingreso del Estudiante";
+            this.grbDatosEstudiante.ResumeLayout(false);
+            this.grbDatosEstudiante.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNombreEstudiante)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbDatosEstudiante;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCedula;
+        private System.Windows.Forms.TextBox txtApellido2;
+        private System.Windows.Forms.TextBox txtApellido1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.DataGridView dgvNombreEstudiante;
+    }
+}
+
